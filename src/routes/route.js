cdb0ca7ffe4f2a82923e4ae7aeca081f97247c28
Prageet05 <prageetsharma05@gmail.com>
2022-10-26 15:32:27 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,10 +2,25 @@ const express = require('express');
 const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
+const bookModel=require('../models/bookModel')
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
+
+
+router.post("/book",async(req,res)=>{
+    let details=req.body;
+    let newDetails=await bookModel.create(details)
+    res.send({myCollections:newDetails})
+})
+
+
+router.get("/demand",async(req,res)=>{
+    let library=await bookModel.find()
+    res.send({collection:library})
+})
+
 
 router.post("/createUser", UserController.createUser  )
 
